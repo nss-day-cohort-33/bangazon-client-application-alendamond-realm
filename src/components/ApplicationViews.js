3e45b0a91096548b0c808ff635c5b36ba47b921a
Mary West <westmarye1219@gmail.com>
2019-10-08 09:46:00 -0500

    deleted commented code

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -126,40 +126,6 @@ const ApplicationViews = () => {
                 }}
             />
 
-            {/* <Route
-                path="/areas" render={props => {
-                    return (
-                        <>
-                            <h1>Areas</h1>
-                            <img className="swings" src={require('./home/swings.jpeg')} alt="My Dog" />
-                        </>
-                    )
-                }}
-            />
-
-            <Route
-                path="/attractions" render={props => {
-                    return (
-                        <>
-                            <h1>Attractions</h1>
-                            <img className="swings" src={require('./home/swings.jpeg')} alt="My Dog" />
-                        </>
-                    )
-                }}
-            />
-
-            <Route
-                path="/myitinerary" render={props => {
-                    return (
-                        <>
-                            <h1>Itinerary</h1>
-                            <img className="swings" src={require('./home/swings.jpeg')} alt="My Dog" />
-                            <MyItinerary {...props} />
-                        </>
-                    )
-                }}
-            /> */}
-
         </React.Fragment>
     )
 }

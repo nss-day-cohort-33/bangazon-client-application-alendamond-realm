2afdafd0cab965f01072f6956d2c3913bbeed4b1
Amber Gooch <agooch91@gmail.com>
2019-10-08 14:05:27 -0500

    added number of items in category and listed products by category

--- src/components/home/ProductTypes.js
+++ src/components/home/ProductTypes.js
@@ -7,20 +7,27 @@ const ProductTypes = props => {
     // Create HTML representation with JSX
     return (
         <>
-
             <h2>Product Categories</h2>
                 <div className="productTypes">
                 {
-                    props.productTypesList.map((item) => {
-                        for(let i=0; i < props.productTypesList.length; i++) {
-                            for (let i=0; i < props.productList.length; i++) {
-                                return <div>
-                                    {item.product_type.name}
-                                    {item.name}
-                                </div>
-
-                            }
-                        }
+                    props.productTypesList.map((type) => {
+                        return (
+                            <div>
+                                <strong>{type.name} ({props.productsList.filter((product) => {
+                                    return type.id === product.product_type_id}).length})</strong>
+                                {props.productsList.filter((product) => {
+                                    return type.id === product.product_type_id})
+                                    .slice(0, 4).map((product) => {
+                                        console.log(product)
+                                        return (
+                                           <li>
+                                               {product.name}
+                                           </li>
+                                       )
+                                    })
+                               }
+                            </div>
+                        )
                     })
                 }
                 </div>

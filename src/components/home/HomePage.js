a0faafdf4275059336496415f90025a81f09e93a
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-10-09 12:42:10 -0500

    fixed the look

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,24 +6,23 @@ const HomePage = props => {
     const [products, setProducts] = useState([])
 
     const get20Products = ()=> {
-        APIManager.get20items("products")
+        APIManager.getAll('products')
             .then((productItem) => {
                 setProducts(productItem)
             })
     }
 
  useEffect(get20Products, [])
+
  return (
      <>
      <h1> Welcome to Bangazon</h1>
      <h4> Here are some of our new items</h4>
      {
-     products.map(item =>{
+     products.slice(-20).map(item =>{
             return(<div key={item.id} className={`productId-${item.id}`}>
-                 <p>{item.name}</p> <p>${item.price}</p>
                 <Link className="nav-link" to={`/products/${item.id}`}>
-
-                <p>Details</p>
+                <p>{item.name}</p>
                 </Link>
                 </div>
                 )

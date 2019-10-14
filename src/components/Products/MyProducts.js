3b0c3322681eec088ab8924dd13d2f9b575a2051
scottsilver <scottsilver47@gmail.com>
2019-10-14 11:57:17 -0500

    wrote fetch call to get products by user

--- src/components/Products/MyProducts.js
+++ src/components/Products/MyProducts.js
@@ -1,20 +1,20 @@
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import APIManager from "../../modules/APIManager";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import APIManager from "../../modules/APIManager";
 
 // //Author: Scott Silver
 // //Purpose: Allow a user to view full list of products they have for sale.
 // //Methods: GET, DELETE
 
-// const MyProducts = props => {
-//   const [myProducts, setMyProducts] = useState([]);
+const MyProducts = props => {
+  const [myProducts, setMyProducts] = useState([]);
 
 //   // Gets a single product type by its product type id and sets state
-// //   const getMyProducts = () => {
-// //     APIManager.get("myProducts", props.match.params.myProductId).then(
-// //       setMyProducts
-// //     );
-// //   };
+  const getMyProducts = () => {
+    APIManager.getMy("myproducts", props.match.params.myproducts).then(
+      setMyProducts
+    );
+  };
 
 //   // Gets all products and sets state
 //   // const getProducts = () => {
@@ -23,38 +23,38 @@
 // };
 
 // // Create useEffect()
-// useEffect(() => {
-//   getMyProducts();
-//   console.log(myProducts)
-// }, []);
+useEffect(() => {
+  getMyProducts();
+  console.log(myProducts)
+}, []);
 
 //     // Create HTML representation with JSX
-//     return (
-//         <>
-//             <h1>{singleType.name}</h1>
-//             <br />
-//             <div className="product-cards">
-//                 {productsList.filter((product) => {
-//                     return singleType.id === product.product_type_id}).map((product) => {
-//                         return (
-//                             <div key={product.id} className="card">
-//                                 <Link key={product.id} className="card-title" to={`/products/${product.id}`}>
-//                                     <h5><strong>{product.name}</strong></h5>
-//                                 </Link>
-//                                 <p className="card-body">
-//                                     ${product.price}
-//                                     <br />
-//                                     Quantity: <em>{product.quantity}</em>
-//                                 </p>
+    return (
+        <>
+            <h1>{myProducts}</h1>
+           <br />
+            {/* <div className="product-cards">
+                 {productsList.filter((product) => {
+                    return myProduct.id === user_id}).map((product) => {
+                        return (
+                             <div key={myProduct.id} className="card">
+                                 <Link key={product.id} className="card-title" to={`/products/${product.id}`}>
+                                     <h5><strong>{product.name}</strong></h5>
+                                 </Link>
+                                 <p className="card-body">
+                                     ${product.price}
+                                     <br />
+                                     Quantity: <em>{product.quantity}</em>
+                                 </p>
 
-//                             </div>
-//                         )
-//                     })
-//                 }
-//             </div>
-//         </>
-//     )
+                           </div>
+                        )
+                    })
+               }
+             </div> */}
+        </>
+    )
 
-// }
+}
 
-// export default MyProducts;
+export default MyProducts;

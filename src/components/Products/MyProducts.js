9fa1e5ee736553b1efc0b5832dba300a1bf35d97
scottsilver <scottsilver47@gmail.com>
2019-10-14 15:40:29 -0500

    fixed delete with anonymous function for onclick

--- src/components/Products/MyProducts.js
+++ src/components/Products/MyProducts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import APIManager from "../../modules/APIManager";
-import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
 
 // //Author: Scott Silver
 // //Purpose: Allow a user to view full list of products they have for sale.
@@ -9,70 +9,69 @@ import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 const MyProducts = props => {
   const [myProducts, setMyProducts] = useState([]);
-  const { isAuthenticated } = useSimpleAuth()
+  const { isAuthenticated } = useSimpleAuth();
 
   // this fetch call gets all the payment types that are specific to this user. It takes a query param of customer id, which we have saved in local storage. That's why it isn't being passed in as an argument
 
   const getMyProducts = () => {
-      if (isAuthenticated()) {
-        fetch(`http://localhost:8000/products?customer=${localStorage.getItem("user_id")}`, {
-            "method": "GET",
-            "headers": {
-              "Accept": "application/json",
-              "Content-Type": "application/json",
-              "Authorization": `Token ${localStorage.getItem("token")}`
-            },
-          }).then(e => e.json())
-          .then(setMyProducts)
-        }
-      }
-
-
-// // Create useEffect()
-useEffect(() => {
-  getMyProducts();
-}, []);
-
-const deleteMyProduct = () => {
     if (isAuthenticated()) {
-        fetch(`http://localhost:8000/myproducts/${props.myproduct.id}`, {
-            "method": "DELETE",
-            "headers": {
-                "Authorization": `Token ${localStorage.getItem("bangazon_token")}`
-            }
-        })
-            .then(props.getPaymentTypes)
-
+      fetch(
+        `http://localhost:8000/products?customer=${localStorage.getItem(
+          "user_id"
+        )}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Token ${localStorage.getItem("token")}`
+          }
+        }
+      )
+        .then(e => e.json())
+        .then(setMyProducts);
     }
-}
+  };
 
-//     // Create HTML representation with JSX
-console.log(myProducts)
-    return (
-        <>
+  const deleteMyProduct = id => {
+    if (isAuthenticated()) {
+      fetch(`http://localhost:8000/products/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Token ${localStorage.getItem("token")}`
+        }
+      }).then(getMyProducts);
+    }
+  };
 
-        <h1>My Products</h1>
-        {myProducts.map(myproduct => {
-          console.log("myproduct", myproduct);
-          return (
-            <div className="card">
-              <ul>
-                <li>{myproduct.name}</li>
-                <li>{myproduct.price}</li>
-                <li>Description: {myproduct.description}</li>
-                <li>Quantity: {myproduct.quantity}</li>
-                <br/>
-                <button onClick={deleteMyProduct}>Delete</button>
-              </ul>
-            </div>
-          );
-        })}
+  // // Create useEffect()
+  useEffect(() => {
+    getMyProducts();
+  }, []);
 
-
-
-        </>
-    )
-
-}
+  //     // Create HTML representation with JSX
+  console.log(myProducts);
+  return (
+    <>
+      <h1>My Products</h1>
+      {myProducts.map(myproduct => {
+        return (
+          <div className="card">
+            <ul>
+              <li>{myproduct.name}</li>
+              <li>{myproduct.price}</li>
+              <li>Description: {myproduct.description}</li>
+              <li>Quantity: {myproduct.quantity}</li>
+              <br />
+              <button onClick={() => deleteMyProduct(myproduct.id)}>
+                Delete
+              </button>
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default MyProducts;

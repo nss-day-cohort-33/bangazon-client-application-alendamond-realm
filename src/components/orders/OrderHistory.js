01b731d5e5a575102f89baa70158d6cfcdbc39c0
Amber Gooch <agooch91@gmail.com>
2019-10-16 14:21:21 -0500

    added order total and formatting

--- src/components/orders/OrderHistory.js
+++ src/components/orders/OrderHistory.js
@@ -18,6 +18,19 @@ const OrderHistory = props => {
     .then(setOrders)
   }
 
+//   const getMyOrders = () => {
+//     fetch("http://localhost:8000/orders?orderlist=false", {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//         "Authorization": `Token ${localStorage.getItem("token")}`
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(setOrders)
+//   }
+
   useEffect(getMyOrders, [])
 
   console.log(allOrders)
@@ -26,7 +39,9 @@ const OrderHistory = props => {
         <div className="order-items">
             <h2>Order History</h2>
                 {
-                    allOrders.map(order => {
+                    allOrders.filter((order) => {
+                        return order.payment_type !== null})
+                    .map(order => {
                         return (
                             <div key={order.id}>
                                 <a href={`/orders/${order.id}`}><h5>Order #{order.id}</h5></a>
